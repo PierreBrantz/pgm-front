48eafd04236783a9215038f064e30f6f9faddf9d
root <root@pan.voo.be>
2021-08-31 00:20:21 +0200

    url problem

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081/";
+//axios.defaults.baseURL = "http://localhost:8081/";
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
